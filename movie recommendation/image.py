182ec789d8909baf1f1525f8994f2ef68e54cd6f
bhairavn <bhairavn19@gmail.com>
2021-04-15 15:46:42 +0530

    filter and streaming site integrated

--- movie recommendation/image.py
+++ movie recommendation/image.py
@@ -2,33 +2,33 @@ import random
 import urllib.request
 import imdb
 import psycopg2
-from flask_mail import Mail
+# from flask_mail import Mail
 
 
-from flask_mail import Message
+# from flask_mail import Message
 
-import smtplib
+# import smtplib
 
-sender = 'bhairavn10@gmail.com'
-receivers = ['bhairavn10@gmail.com']
+# sender = 'bhairavn10@gmail.com'
+# receivers = ['bhairavn10@gmail.com']
 
-message = """From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
-Subject: SMTP e-mail test
+# message = """From: From Person <from@fromdomain.com>
+# To: To Person <to@todomain.com>
+# Subject: SMTP e-mail test
 
-This is a test e-mail message.
-"""
+# This is a test e-mail message.
+# """
 
-try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, receivers, message)         
-   print ("Successfully sent email")
-except SMTPException:
-   print ("Error: unable to send email")
+# try:
+#    smtpObj = smtplib.SMTP('localhost')
+#    smtpObj.sendmail(sender, receivers, message)         
+#    print ("Successfully sent email")
+# except SMTPException:
+#    print ("Error: unable to send email")
 
-# ia=imdb.IMDb()
+ia=imdb.IMDb()
 # index()
-# top = ia.get_top250_movies()
+top = ia.get_top250_movies()
 
 # def download_image(url):
 #     name = random.randrange(1,100)
@@ -36,30 +36,37 @@ except SMTPException:
 #     urllib.request.urlretrieve(url,fullname)     
 
 
-# con = psycopg2.connect(
-#     host = "127.0.0.1",
-#     database = "movierecommender",
-#     user = "postgres",
-#     password = "admin",
-#     port="5432")
+con = psycopg2.connect(
+    host = "127.0.0.1",
+    database = "movierecommender",
+    user = "postgres",
+    password = "admin",
+    port="5432")
 
-# cur=con.cursor()
-
+cur=con.cursor()
+# for i in  top:
+#    st="desktop"+".jpg"
+#    urllib.request.urlretrieve(ia.get_movie(i.movieID).data['cover url'],st)
+#    print (st)
 
 # print()
 # # print("hello")
 # # print (ia.get_movie(top[0].movieID).data['cover url'])
-# import urllib.request
-# j=int(0)
-# # for i in top:
-# #     # print(i['title'],type(i['year']),type(str(i['rating'])))
-# #     st='assets/images/' + str(j) + '.jpg'
-# #     # a=ia.get_movie(i.movieID).data['genres']
-# #     # print(a)
-# #     cur.execute("INSERT INTO public.moviedatabase (title,year,rating,image) VALUES (%s,%s::bigint,%s,%s)",(i['title'],i['year'],i['rating'],st));
-# #     con.commit()
-# #     j+=1
-# #     print(j)
+import urllib.request
+j=int(0)
+for i in top:
+   # print(i['title'],type(i['year']),type(str(i['rating'])))
+   s='assets/images/' + str(j) + '.jpg'
+   a=ia.get_movie(i.movieID).data['genres']
+   st="images/"+str(j)+".jpg"
+   urllib.request.urlretrieve(ia.get_movie(i.movieID).data['cover url'],st)
+   gn = ','.join(a)
+   print(ia.get_movie(i.movieID).data['cover url'])
+   print(i['title'], i['year'], i['rating'], gn, s, j)
+   cur.execute("INSERT INTO public.moviedatabase (title,year,rating,image,genre) VALUES (%s,%s::bigint,%s,%s,%s)",(i['title'],i['year'],i['rating'],s,gn));
+   con.commit()
+   j+=1
+# print(j)
 # cur.execute("select * from public.moviedatabase")
 # mdata = cur.fetchall()
 # for i in mdata:
--- movie recommendation/user.py
+++ movie recommendation/user.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,flash,session,url_for,g,redirect
+from flask import Flask,render_template,request,flash,session,url_for,g,redirect,jsonify
 import imdb
 from movie import webscrap
 from passlib.hash import sha256_crypt
@@ -7,6 +7,10 @@ from math import ceil
 from textanalysis import returnlabel
 import smtplib
 from flask_mail import Mail
+import json
+import re
+from stream_sites import stream_link
+
 app=Flask(__name__)
 
 con = psycopg2.connect(
@@ -20,21 +24,373 @@ cur=con.cursor()
 cur.execute("select * from public.moviedatabase")
 mdata = cur.fetchall()
 n=len(mdata)
+temp=mdata.copy()
 nslides= n//4 +ceil((n/4)-(n//4))
 
-top = webscrap()
+# top = webscrap()
 a={"admin@gmail.com":"qwerty"}
-ia1=imdb.IMDb()
+# ia1=imdb.IMDb()
+lst=[]
 
-@app.route('/')
+@app.route('/filter',methods=['POST','GET'])
+def filter():
+    
+    if request.get_data().decode('utf-8').strip()=="Action":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Action", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Comedy":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Comedy", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        # return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Drama":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Drama", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Horror":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Horror", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Romance":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Romance", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        # return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata,l=len(temp)),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Documentry":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Documentry", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Animation":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Animation", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    else :
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            temp.append(mpdata[i])
+            count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,l=len(temp),len=n,ns=4)
+    
+    return render_template('index.html',posts1=mdata,posts2=temp,len=n,l=len(temp),ns=4)
+
+
+
+@app.route('/filter2',methods=['POST','GET'])
+def filter2():
+    
+    if request.get_data().decode('utf-8').strip()=="Action":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Action", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+        return redirect(url_for('user'))
+    elif request.get_data().decode('utf-8').strip()=="Comedy":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Comedy", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        # return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Drama":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Drama", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Horror":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Horror", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Romance":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Romance", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        # return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata,l=len(temp)),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Documentry":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Documentry", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    elif request.get_data().decode('utf-8').strip()=="Animation":
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            txt = mpdata[i][4]
+            x = re.findall("Animation", txt)
+            if len(x)!=0:
+                temp.append(mpdata[i])
+                count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
+    else :
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+
+        print("///////////////////////////////////////////////////////")
+        print("*************************",request.get_data().decode('utf-8'))
+        print(type(temp))
+        mpdata=mdata.copy()
+        temp.clear() 
+        count=0
+        for i in range(n):
+            temp.append(mpdata[i])
+            count+=1      
+
+        print(len(temp),len(mdata))
+
+        return redirect(url_for('index'))
+
+        return render_template('index.html',posts1=mdata,posts2=temp,l=len(temp),len=n,ns=4)
+    
+    return render_template('index.html',posts1=mdata,posts2=temp,len=n,l=len(temp),ns=4)
+
+
+
+
+@app.route('/',methods=['POST','GET'])
 def index():
     session.pop("user",None)
+    # print(json.dumps(request.form['data']))
+    print(len(mdata))
     
-    print(mdata[0])
-    
-    print(type(nslides))
-    
-    return render_template('index.html',posts=mdata,len=n,ns=4)
+    # print(type(nslides))
+    return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
 
     
 @app.route('/login', methods=['POST',"GET"])
@@ -49,10 +405,18 @@ def login():
         print("email ",email,"pass ",passwo)
         try:
             cur.execute("select email from public.user Where email = '" + email +"'")
-            userdata = cur.fetchone()[0]
+            db_userdata = cur.fetchone()
+            userdata=""
+            if db_userdata:
+                userdata = db_userdata[0]
+            if userdata=='':
+                return render_template("login.html")
 
             cur.execute("select passw from public.user Where email = '" + email +"'")
-            passw = cur.fetchone()[0]
+            db_passw = cur.fetchone()
+            passw=""
+            if db_passw:
+                passw = db_passw[0]
         
         except psycopg2.Error as e:
             t_message = e
@@ -70,6 +434,7 @@ def login():
                 session['user']=email
                 d = get_details(email)
                 print(type(d))
+
                 return redirect(url_for('user'))
                 # return render_template("user.html")
                 # return redirect(url_for('user'))
@@ -166,13 +531,38 @@ def change():
 def forgot():
     return render_template('forgot.html')
 
+@app.route('/movielink',methods=['POST','GET'])
+def movielink():
+    displaypic=request.form.get('displaypic')
+    name=request.form.get('name')
+    year=request.form.get('year')
+    rating=request.form.get('rating')
+    genre=request.form.get('genre')
+    print(displaypic,"\n",name,"\n",year,"\n",rating,"\n",genre,"\n","hello")
+    lst.clear()
+    lst.append(displaypic)
+    lst.append(name)
+    lst.append(year)
+    lst.append(rating)
+    lst.append(genre)
+    return redirect(url_for('stream'))
+    return render_template('movielink.html',displaypic=displaypic,name=name,year=year,rating=rating,genre=genre)
+
+@app.route('/movie')
+def stream():
+    print("dfgfgfggd")
+    movie_name = lst[1]
+    movie_name = movie_name.strip()
+    mlink = stream_link(movie_name)
+    return render_template('movielink.html',displaypic=lst[0],name=lst[1],year=lst[2],rating=lst[3],genre=lst[4],mstream=mlink)
 
 @app.route('/user')
 def user():
     if g.user:
         det=get_details(session['user'])
-        return render_template('user.html',user=session['user'],det=det,posts=mdata,len=n,ns=4)
-    return render_template('index.html',posts=mdata)
+        ### colaborative model (pass userid and movie id format from code ...) 
+        return render_template('user.html',user=session['user'],det=det,posts1=mdata,posts2=temp,len=n,ns=4)
+    return render_template('index.html',posts1=mdata,posts2=temp,len=len(mdata),l=len(temp),ns=4)
 
 def update_db(d,a):
     cur.execute("select * from public.user Where email = '" + session['user'] +"'")
@@ -300,3 +690,22 @@ app.secret_key="jzdbvkjbhjvhjzfshfzhjfbzhxbfhzb"
 app.run(debug=True)
     
 
+'''
+if ($i==1)
+    	{console.log("Action");
+		qurl='http://127.0.0.1:5000/'
+	$.ajax({
+            type: "POST",
+            cache: false,
+            data:"Action",
+            url: qurl,
+            dataType: "json",
+            success: function(data) { 
+                console.log(data);                    
+            },
+            error: function(jqXHR) {
+                alert("error: " + jqXHR.status);
+                console.log(jqXHR);
+            }
+        })
+'''
